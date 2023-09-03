e2ce7316b00ff3743675894626eddad7cde18700
Alina Vasileva <alinav122123@yandex.ru>
2023-09-03 14:54:53 +0700

    style(components): uniformity of styles after "prettier"

--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
