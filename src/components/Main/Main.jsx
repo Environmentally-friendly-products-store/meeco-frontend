3fa55d553f18f008f1262ae71cb801507bc08b10
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2023-09-05 20:23:59 +0800

    refactor(main): убрать ProductCard перед блоками SectionCard

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,5 @@
 import Promo from '../Promo/Promo';
 
-import ProductCard from '../ProductCard/ProductCard';
 import AboutProject from '../AboutProject/AboutProject';
 import CardSection from '../CardSection/CardSection';
 import Novelties from '../Novelties/Novelties';
@@ -11,21 +10,6 @@ export default function Main() {
     <>
       <Promo />
 
-
-      <ProductCard
-        /* Нужно указать, используется ли компонент на mainpage. */
-        isUsedOnMainPage={true}
-        /* Через пропс нужно передать имя секции в виде строки,
-        в которой будет использоваться компонент. на данный момент есть novelty(новинки),
-         popular и catalog. В зависимости от выбранного значения, будут применяться разные стили */
-        sectionWhereUsed={'novelty'}
-        price={1000}
-        /* image сейчас подключен напрямую в компоненте ProductCard, это временное решение */
-        /* image={image} */
-        name={'Название товара'}
-        brand={'Бренд'}
-      />
-          
       <CardSection title="Новые поступления">
         <Novelties />
       </CardSection>
