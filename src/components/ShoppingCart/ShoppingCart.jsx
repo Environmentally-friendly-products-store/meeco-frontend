be4048ec624c8442cd66a30f4fb51adb4f284d12
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2023-09-08 13:45:27 +0800

    feat(emptycart): добавил компонент EmptyCart

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -7,6 +7,7 @@ import { temporaryProductsArray } from '../../utils/functions/temporaryObjectArr
 import { sortProducts } from '../../utils/functions/sortProducts';
 import { useState } from 'react';
 import ShoppingCardItem from '../ShoppingCartItem/ShoppingCardItem';
+import EmptyCart from '../EmptyCart/EmptyCart';
 
 function ShoppingCart() {
   const products = sortProducts(temporaryProductsArray, 2);
@@ -19,36 +20,43 @@ function ShoppingCart() {
   /* Пока не понял, как передавать корректные данные в секцию c общей суммой */
 
   return (
-    <main className="shopping-cart">
+    <main
+      className={`shopping-cart ${
+        products.length < 1 ? 'shopping-cart_style_empty' : ''
+      }`}
+    >
       <h1 className="shopping-cart__title">Корзина</h1>
+      {products.length < 1 ? (
+        <EmptyCart />
+      ) : (
+        <div className="shopping-cart__products-block">
+          <button className="shopping-cart__button shopping-cart__button_style_reset">
+            Удалить все
+          </button>
 
-      <div className="shopping-cart__products-block">
-        <button className="shopping-cart__button shopping-cart__button_style_reset">
-          Удалить все
-        </button>
+          <ul className="shopping-cart__products-list">
+            {products.map((product) => (
+              <ShoppingCardItem
+                key={product.id}
+                product={product}
+                onTotalPriceChange={onTotalPriceChange}
+              />
+            ))}
+          </ul>
 
-        <ul className="shopping-cart__products-list">
-          {products.map((product) => (
-            <ShoppingCardItem
-              key={product.id}
-              product={product}
-              onTotalPriceChange={onTotalPriceChange}
-            />
-          ))}
-        </ul>
+          <div className="shopping-cart__total-block">
+            <p className="shopping-cart__total-block-title">Итого</p>
+            <p className="shopping-card__product-price shopping-card__product-price_style_sum">
+              {totalPrice} ₽
+            </p>
+          </div>
 
-        <div className="shopping-cart__total-block">
-          <p className="shopping-cart__total-block-title">Итого</p>
-          <p className="shopping-card__product-price shopping-card__product-price_style_sum">
-            {totalPrice} ₽
-          </p>
+          <NavLink className="shopping-cart__place-order-button" to="/">
+            {/* пока что нет компонента с оформлением заказа, поэтому ссылка на Main */}
+            Оформить заказ
+          </NavLink>
         </div>
-
-        <NavLink className="shopping-cart__place-order-button" to="/">
-          {/* пока что нет компонента с оформлением заказа, поэтому ссылка на Main */}
-          Оформить заказ
-        </NavLink>
-      </div>
+      )}
     </main>
   );
 }
