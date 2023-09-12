c54cf50e29338fc6f8bedc27653fcd0e73b9ac61
Anatoly Karpov <karpik-ne@yandex.ru>
2023-09-12 12:37:36 +0700

    Fix состояния ссылок

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -21,7 +21,10 @@ function Order() {
           <DeliveryAdress />
           <Recipient />
           <div className="order__link">
-            <Link to="/shopping-cart" className="order__link-text">
+            <Link
+              to="/shopping-cart"
+              className="order__link-text selectable-link"
+            >
               <img className="order__arrow" src={arrowleft} alt="Стрелка" />
               Вернуться в корзину
             </Link>
@@ -38,7 +41,7 @@ function Order() {
           </button>
           <p className="order__politic">
             Нажимая кнопку, вы соглашаетесь с{' '}
-            <Link className="order__politic-text" to="/">
+            <Link className="order__politic-text selectable-link" to="/">
               Политикой конфиденциальности
             </Link>{' '}
           </p>
--- src/components/ThanksForOrder/ThanksForOrder.jsx
+++ src/components/ThanksForOrder/ThanksForOrder.jsx
@@ -31,7 +31,7 @@ function ThanksForOrder() {
           <DeliveryAdress />
           <Recipient />
           <div className="order__link">
-            <Link to="/catalog" className="order__link-text">
+            <Link to="/catalog" className="order__link-text selectable-link">
               <img className="order__arrow" src={arrowleft} alt="Стрелка" />
               Продолжить покупки
             </Link>
