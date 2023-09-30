1af925cfd88b8472569f096df3168f3fe8c93e04
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2023-09-30 20:02:57 +0800

    feat(privacypolicy): черновой варинт новой политики конфеденциальности

--- src/components/InfoBlock/InfoBlock.jsx
+++ src/components/InfoBlock/InfoBlock.jsx
@@ -1,10 +1,27 @@
 import './InfoBlock.css';
 import infoblock from '../../images/termsofuse-frame.png';
 
-export default function InfoBlock({ title, id, children, showImage = true }) {
+export default function InfoBlock({
+  title,
+  id,
+  children,
+  showImage = true,
+  hasAlternativeTitleStyles,
+}) {
   return (
-    <div className="infoblock">
-      <h2 className="infoblock__title" id={id}>
+    <div
+      className={`infoblock ${
+        hasAlternativeTitleStyles ? 'infoblock_style_privacy-policy' : ''
+      }`}
+    >
+      <h2
+        className={`infoblock__title ${
+          hasAlternativeTitleStyles
+            ? 'infoblock__title_style_privacy-policy'
+            : ''
+        }`}
+        id={id}
+      >
         {title}
       </h2>
       {showImage && (
