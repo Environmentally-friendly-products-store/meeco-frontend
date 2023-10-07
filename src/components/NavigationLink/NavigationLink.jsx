f02babd79f81ca6b42dbb16cbc03b36d5c682d93
Sergey Skotselyas <sergey.sckotcelias@yandex.ru>
2023-10-07 22:00:09 +0800

    fix(navigationlink): исправил alt в NavigationLink для скринридеров

--- src/components/NavigationLink/NavigationLink.jsx
+++ src/components/NavigationLink/NavigationLink.jsx
@@ -5,7 +5,11 @@ import './NavigationLink.css';
 function NavigationLink({ path, text, image, count }) {
   return (
     <Link to={path} className="navigation-link selectable-button">
-      <img className="navigation-link__image" src={image} alt={text}></img>
+      <img
+        className="navigation-link__image"
+        src={image}
+        alt={`Кнопка ${text}`}
+      ></img>
       <span className="navigation-link__text">{text}</span>
       {count && <span className="navigation-link__count_active ">{count}</span>}
     </Link>
