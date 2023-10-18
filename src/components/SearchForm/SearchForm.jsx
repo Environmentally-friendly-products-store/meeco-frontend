c404e9d974eb4b074bcfa867f2b7db1c93e9cf6b
Alina Vasileva <alinav122123@yandex.ru>
2023-10-18 21:43:12 +0700

    feat(searchform): restyle searchform

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,21 +4,19 @@ import './SearchForm.css';
 function SearchForm() {
   return (
     <form className="search-form" method="post">
-      <div className="search-form__string">
-        <input
-          className="search-form__input"
-          type="text"
-          placeholder="Найти"
-          minLength={3}
-          maxLength={255}
-          required
-        />
-        <button
-          className="search-form__button selectable-button"
-          type="submit"
-          aria-label="Кнопка поиска товаров"
-        ></button>
-      </div>
+      <input
+        className="search-form__input"
+        type="text"
+        placeholder="Найти"
+        minLength={3}
+        maxLength={255}
+        required
+      />
+      <button
+        className="search-form__button selectable-button"
+        type="submit"
+        aria-label="Кнопка поиска товаров"
+      ></button>
       <span className="search-form__error"></span>
     </form>
   );
